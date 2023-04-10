2793da9bfaf9c66a84f0c657b17d81b42fcb328c
Aline Santos <aline.santosx91@gmail.com>
2023-04-09 23:44:53 -0300

    registra arquivo de logs

--- hello/hello.go
+++ hello/hello.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -85,8 +86,10 @@ func testarSite(site string) {
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site", site, "foi carregado com sucesso!!")
+		registrarLog(site, true)
 	} else {
 		fmt.Println("Site", site, "está com problemas. Status code:", resp.StatusCode)
+		registrarLog(site, false)
 	}
 }
 
@@ -112,4 +115,20 @@ func lerSitesDoArquivo() []string {
 
 	arquivo.Close()
 	return sites
+}
+
+func registrarLog(site string, status bool) {
+
+	arquivo, err := os.OpenFile("logs.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	now := time.Now().Format("02/01/2006 15:04:05")
+
+	arquivo.WriteString(now + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+
 }
